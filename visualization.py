fc8e180b627fa8f091b787a4f4b9fd8456e7df03
stevenswo <steven.apps.paid@gmail.com>
2023-03-18 23:10:16 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,10 +1,15 @@
-import panda as pd;
+import os;
+import pandas as pd;
 import numpy as np;
 import matplotlib.pyplot as plt;
 
-df = pd.read_csv(r'/content/Fitness_trackers_updated.csv')
+data = open("assets/data/Fitness_trackers_updated.csv")
+df = pd.read_csv(data)
 dataBrand = np.array(df['Brand Name'])
 uniqueBrand = np.unique(dataBrand)
 brandCount = []
 for item in uniqueBrand:
   brandCount.append(len(dataBrand[dataBrand == item]))
+
+#Pie Chart
+plt.pie(brandCount, labels = uniqueBrand)
